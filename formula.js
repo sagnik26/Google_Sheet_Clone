cdd386c21bd38650d811545520c96411b8e2ec21
Sagnik Ghosh <sagnikghosh@Sagniks-MacBook-Air.local>
2023-11-06 16:07:42 +0530

    Added automatic cell update -v1

--- formula.js
+++ formula.js
@@ -10,5 +10,76 @@ for(let i=0; i<rows; i++) {
     }
 }
 
+let formulaBar = document.querySelector(".formula-bar");
+formulaBar.addEventListener("keydown", (e) => {
+    let inputFormula = formulaBar.value;
+    if(e.key === "Enter" && inputFormula) {
+        // If change in formula, break old P-C relation, evaluate new formula, add new P-C relation
+        let address = addressBar.value;
+        let [cell, cellProp] = activecell(address);
+        if(inputFormula !== cellProp.formula) removeChildFromParent(cellProp.formula);
+
+        let evaluatedVal = evaluateFormula(inputFormula);
+
+        // To update UI and cellProp in DB
+        setCellUIAndCellProp(evaluatedVal, inputFormula);
+        addChildToParent(inputFormula);
+        console.log("SHEET", sheetDB);
+    }
+});
+
+function updateChildrenCells(address) {
+    
+}
+
+function addChildToParent(formula) {
+    let childAddress = addressBar.value;
+    let encodedFormula = formula.split(" ");
+    for (let i = 0; i < encodedFormula.length; i++) {
+        let asciiValue = encodedFormula[i].charCodeAt(0);
+        if (asciiValue >= 65 && asciiValue <= 90) {
+            let [parentCell, parentCellProp] = activecell(encodedFormula[i]);
+            parentCellProp.children.push(childAddress);
+        }
+    }
+}
+
+function removeChildFromParent(formula) {
+    let childAddress = addressBar.value;
+    let encodedFormula = formula.split(" ");
+    for (let i = 0; i < encodedFormula.length; i++) {
+        let asciiValue = encodedFormula[i].charCodeAt(0);
+        if (asciiValue >= 65 && asciiValue <= 90) {
+            let [parentCell, parentCellProp] = activecell(encodedFormula[i]);
+            let idx = parentCellProp.children.indexOf(childAddress);
+            parentCellProp.children.splice(idx, 1);
+        }
+    }
+}
+
+function evaluateFormula(formula) {
+    let encodedFormula = formula.split(" ");
+    for(let i=0; i<encodedFormula.length; i++) {
+        let asciiValue = encodedFormula[i].charCodeAt(0);
+        if(asciiValue >= 65 && asciiValue <= 90) {
+            let [cell, cellProp] = activecell(encodedFormula[i]);
+            encodedFormula[i] = cellProp.value;
+        }
+    }
+    let decodedFormula = encodedFormula.join(" ");
+    return eval(decodedFormula);
+}
+
+function setCellUIAndCellProp(evaluatedVal, formula) {
+    let address = addressBar.value;
+    let [cell, cellProp] = activecell(address);
+
+    // UI update
+    cell.innerText = evaluatedVal;
+
+    // DB update
+    cellProp.value = evaluatedVal;
+    cellProp.formula = formula;
+}
 
 
